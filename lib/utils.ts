3ca1ecc4487d4d64724d70dc97a1f8d66dcb57fb
Engu Luttudo <91648695+Luttudo@users.noreply.github.com>
2024-06-02 17:40:41 -0300

    implementa o form de sign up/in com validação
    https://ui.shadcn.com/docs/components/form
    
    validação feita com zod

--- lib/utils.ts
+++ lib/utils.ts
@@ -195,6 +195,13 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
+
+export const authFormSchemaBKP = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+})
+
+
 export const authFormSchema = (type: string) => z.object({
   // sign up
   firstName: type === 'sign-in' ? z.string().optional() : z.string().min(3),
