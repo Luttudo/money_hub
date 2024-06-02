41a91b4f648ec63c34b608c3935c957be9fa6cde
Engu Luttudo <91648695+Luttudo@users.noreply.github.com>
2024-06-02 18:10:45 -0300

    implementa funções para recolher informações do form
    retira o console log

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,11 +21,13 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const FormSchema = z.object({
     email: z.string().email(),
 })
 const AuthForm = ({ type }: {type: string}) => {
+    const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setisLoading] = useState(false);
 
@@ -41,13 +43,35 @@ const AuthForm = ({ type }: {type: string}) => {
     })
     
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         setisLoading(true)
-        console.log(values)
-        setisLoading(false);
-    }
+        
+        try {
+            //sign up with appwrite & create plaid token
+
+            if(type === "sign-up") {
+
+                //const newUser = await signUp(userData);
+
+                //setUser(newUser);
+            }
+
+            if(type === 'sign-in') {
+                // const response = await signIn({
+                //   email: data.email,
+                //   password: data.password,
+                // })
+
+                // if(response) router.push('/')
+                }
+              } catch (error) {
+                console.log(error);
+              } finally {
+                setisLoading(false);
+              }
+            }
 
 
   return (
